ff0d0069d31dcf99ec39cd66a035170746db7bb0
Zoe <zinkies@outlook.com>
2023-09-19 19:09:12 -0400

    Initial Movement
    Initial movement implementation using the UE5EIS.

--- Source/ActionRoguelike/Private/SBCharacter.cpp
+++ Source/ActionRoguelike/Private/SBCharacter.cpp
@@ -4,25 +4,60 @@
 
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
-
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
 
 // Sets default values
 ASBCharacter::ASBCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	this->bUseControllerRotationPitch = true;
+	
 	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
 	SpringArmComp->SetupAttachment(RootComponent);
+	
 	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
 	CameraComp->SetupAttachment(SpringArmComp);
+	
+}
+
+void ASBCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller)
+	{
+		AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+		AddMovementInput(GetActorRightVector(), MovementVector.X);
+	}
+}
+
+void ASBCharacter::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (Controller)
+	{
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y );
+	}
 }
 
 // Called when the game starts or when spawned
 void ASBCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	//Add the input mapping context.
+	if (const APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
 }
 
 // Called every frame
@@ -35,7 +70,23 @@ void ASBCharacter::Tick(float DeltaTime)
 // Called to bind functionality to input
 void ASBCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	Super::SetupPlayerInputComponent(PlayerInputComponent); // TODO: Is this needed?
 
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		//Looking
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ASBCharacter::Look);
+		
+		//Moving
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ASBCharacter::Move);
+
+		//Jumping
+		//TODO: Implement Jumping.
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("'%s' Failed to find an Enhanced Input Component. This character is not set up to use legacy inputs."), *GetNameSafe(this));
+	}
+	
 }
 
--- Source/ActionRoguelike/Public/SBCharacter.h
+++ Source/ActionRoguelike/Public/SBCharacter.h
@@ -6,18 +6,53 @@
 #include "GameFramework/Character.h"
 #include "SBCharacter.generated.h"
 
+struct FInputActionValue;
 class UCameraComponent;
 class USpringArmComponent;
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class ACTIONROGUELIKE_API ASBCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+	/** Spring Arm component added in Lecture 2.3. Used for the 3rd person camera.*/
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	USpringArmComponent* SpringArmComp;
+	
+	/** Camera component added in Lecture 2.3. Used by the SpringArmComp.*/
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	UCameraComponent* CameraComp;
+
+	/** The default input mapping context for the character.*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+	
+	/** The move action. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
+	/** The rotate action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
+
+	/** The jump action.*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* JumpAction;
+	
 public:
 	// Sets default values for this character's properties
 	ASBCharacter();
 
+protected:
+
+	/** Moves the character. Called by the MoveAction function.*/
+	void Move(const FInputActionValue& Value);
+
+	/** Makes the character look around. Called by the LookAction function.*/
+	void Look(const FInputActionValue& Value);
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -28,19 +63,4 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-protected:
-	
-	/**
-	 * Spring Arm component added in Lecture 2.3. Used for the 3rd person camera.
-	 */
-	UPROPERTY(VisibleAnywhere)
-	USpringArmComponent* SpringArmComp;
-
-	
-	/**
-	 * Camera component added in Lecture 2.3. Used by the SpringArmComp.
-	 */
-	UPROPERTY(VisibleAnywhere)
-	UCameraComponent* CameraComp;
 };
